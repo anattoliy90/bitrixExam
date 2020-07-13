68e98d8944e4501f66d8f3157b989c508cf4f064
anattoliy90 <anattoliy90@gmail.com>
2020-07-13 10:42:14 +0300

    logic and view ready

--- local/components/molotok/simplecomp.exam_ex2-70/component.php
+++ local/components/molotok/simplecomp.exam_ex2-70/component.php
@@ -5,6 +5,8 @@ if(!isset($arParams["CACHE_TIME"])) {
 	$arParams["CACHE_TIME"] = 3600;
 }
 
+$count = 0;
+
 if($this->StartResultCache()) {
 	if(!CModule::IncludeModule("iblock")) {
 		$this->AbortResultCache();
@@ -16,7 +18,7 @@ if($this->StartResultCache()) {
 	$catalogItems = [];
 	$newsItems = [];
 	
-	$qSect = CIBlockSection::GetList([], ['IBLOCK_ID' => $arParams['CATALOG_IBLOCK'], 'ACTIVE' => 'Y', '!' . $arParams['PROP_NEWS_LINK'] => false], false, ['ID', 'NAME'], false);
+	$qSect = CIBlockSection::GetList([], ['IBLOCK_ID' => $arParams['CATALOG_IBLOCK'], 'ACTIVE' => 'Y', '!' . $arParams['PROP_NEWS_LINK'] => false], false, ['ID', 'NAME', $arParams['PROP_NEWS_LINK']], false);
 	while ($rSect = $qSect->GetNext()) {
 		$sections[$rSect['ID']] = $rSect;
 	}
@@ -26,13 +28,35 @@ if($this->StartResultCache()) {
 		$catalogItems[] = $rElem;
 	}
 	
-	$qNews = CIBlockElement::GetList([], ['IBLOCK_ID' => $arParams['NEWS_IBLOCK'], 'ACTIVE' => 'Y'], false, false, ['ID', 'NAME']);
+	$qNews = CIBlockElement::GetList([], ['IBLOCK_ID' => $arParams['NEWS_IBLOCK'], 'ACTIVE' => 'Y'], false, false, ['ID', 'NAME', 'ACTIVE_FROM']);
 	while ($rNews = $qNews->GetNext()) {
 		$newsItems[] = $rNews;
 	}
 	
-	echo '<pre>';print_r($newsItems);echo '</pre>';
+	foreach ($newsItems as $newsK => $newsV) {
+		$sectionIds = [];
+		
+		foreach ($sections as $section) {
+			if (in_array($newsV['ID'], $section['UF_NEWS_LINK'])) {
+				$sectionIds[] = $section['ID'];
+				$newsItems[$newsK]['SECTIONS'][] = $section['NAME'];
+			}
+		}
+		
+		foreach ($catalogItems as $item) {
+			if (in_array($item['IBLOCK_SECTION_ID'], $sectionIds)) {
+				$newsItems[$newsK]['ITEMS'][] = $item;
+			}
+		}
+	}
+	
+	$count = count($catalogItems);
+	$arResult['NEWS'] = $newsItems;
 
 	$this->SetResultCacheKeys([]);
 	$this->IncludeComponentTemplate();
 }
+
+$title = GetMessage['COUNT_CATALOG_ITEMS'] . $count;
+
+$APPLICATION->SetTitle($title);

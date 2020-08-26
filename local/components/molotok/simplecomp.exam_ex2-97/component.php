64c1da3edcbc46e71b4c2a0f3ddac00ba29d0ffa
anattoliy90 <anattoliy90@gmail.com>
2020-08-26 10:37:17 +0300

    working on component

--- local/components/molotok/simplecomp.exam_ex2-97/component.php
+++ local/components/molotok/simplecomp.exam_ex2-97/component.php
@@ -10,66 +10,49 @@ if(!Loader::includeModule("iblock"))
 	return;
 }
 
-if(intval($arParams["PRODUCTS_IBLOCK_ID"]) > 0)
-{
+if(intval($arParams["NEWS_IBLOCK_ID"]) > 0 && $this->StartResultCache(false, $USER->GetID())) {
+	// current user
+	$rsUser = CUser::GetByID($USER->GetID());
+	$curUser = $rsUser->Fetch();
 	
-	//iblock elements
-	$arSelectElems = array (
-		"ID",
-		"IBLOCK_ID",
-		"NAME",
-	);
-	$arFilterElems = array (
-		"IBLOCK_ID" => $arParams["PRODUCTS_IBLOCK_ID"],
-		"ACTIVE" => "Y"
-	);
-	$arSortElems = array (
-			"NAME" => "ASC"
-	);
-	
-	$arResult["ELEMENTS"] = array();
-	$rsElements = CIBlockElement::GetList($arSortElems, $arFilterElems, false, false, $arSelectElems);
-	while($arElement = $rsElements->GetNext())
-	{
-		$arResult["ELEMENTS"][] = $arElement;
-	}
-	
-	//iblock sections
-	$arSelectSect = array (
-			"ID",
-			"IBLOCK_ID",
-			"NAME",
-	);
-	$arFilterSect = array (
-			"IBLOCK_ID" => $arParams["PRODUCTS_IBLOCK_ID"],
-			"ACTIVE" => "Y"
-	);
-	$arSortSect = array (
-			"NAME" => "ASC"
-	);
-	
-	$arResult["SECTIONS"] = array();
-	$rsSections = CIBlockSection::GetList($arSortSect, $arFilterSect, false, $arSelectSect, false);
-	while($arSection = $rsSections->GetNext())
-	{
-		$arResult["SECTIONS"][] = $arSection;
-	}
-		
 	// user
 	$arOrderUser = array("id");
 	$sortOrder = "asc";
 	$arFilterUser = array(
-		"ACTIVE" => "Y"
+		"ACTIVE" => "Y",
+		$arParams["AUTHOR_TYPE_PROP_CODE"] => $curUser["UF_AUTHOR_TYPE"],
 	);
 	
 	$arResult["USERS"] = array();
-	$rsUsers = CUser::GetList($arOrderUser, $sortOrder, $arFilterUser); // выбираем пользователей
-	while($arUser = $rsUsers->GetNext())
-	{
+	$rsUsers = CUser::GetList($arOrderUser, $sortOrder, $arFilterUser);
+	while($arUser = $rsUsers->GetNext()) {
 		$arResult["USERS"][] = $arUser;
-	}	
+	}
 	
+	echo "<pre>";print_r($arResult["USERS"]);echo "</pre>";
 	
+	// news
+	$arSelectElems = array (
+		"ID",
+		"IBLOCK_ID",
+		"NAME",
+		"ACTIVE_FROM",
+		"PROPERTY_" . $arParams["AUTHOR_PROP_CODE"],
+	);
+	$arFilterElems = array (
+		"IBLOCK_ID" => $arParams["NEWS_IBLOCK_ID"],
+		"ACTIVE" => "Y",
+	);
+	$arSortElems = array (
+		"NAME" => "ASC",
+	);
+	
+	$rsElements = CIBlockElement::GetList($arSortElems, $arFilterElems, false, false, $arSelectElems);
+	while($arElement = $rsElements->GetNext()) {
+		$arResult["NEWS"][] = $arElement;
+		
+		echo "<pre>";print_r($arElement);echo "</pre>";
+	}
 }
+
 $this->includeComponentTemplate();	
-?>

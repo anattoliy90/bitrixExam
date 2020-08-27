41307ce0f73702341d0a28a449276365eca0b8ae
anattoliy90 <anattoliy90@gmail.com>
2020-08-27 10:39:09 +0300

    working on ex2-97 component

--- local/components/molotok/simplecomp.exam_ex2-97/component.php
+++ local/components/molotok/simplecomp.exam_ex2-97/component.php
@@ -22,14 +22,16 @@ if(intval($arParams["NEWS_IBLOCK_ID"]) > 0 && $this->StartResultCache(false, $US
 		"ACTIVE" => "Y",
 		$arParams["AUTHOR_TYPE_PROP_CODE"] => $curUser["UF_AUTHOR_TYPE"],
 	);
+	$arSelect = [
+		"FIELDS" => ["ID", "LOGIN"],
+		"SELECT" => [$arParams["AUTHOR_TYPE_PROP_CODE"]],
+	];
 	
 	$arResult["USERS"] = array();
-	$rsUsers = CUser::GetList($arOrderUser, $sortOrder, $arFilterUser);
+	$rsUsers = CUser::GetList($arOrderUser, $sortOrder, $arFilterUser, $arSelect);
 	while($arUser = $rsUsers->GetNext()) {
 		$arResult["USERS"][] = $arUser;
 	}
-	
-	echo "<pre>";print_r($arResult["USERS"]);echo "</pre>";
 	
 	// news
 	$arSelectElems = array (
@@ -42,17 +44,40 @@ if(intval($arParams["NEWS_IBLOCK_ID"]) > 0 && $this->StartResultCache(false, $US
 	$arFilterElems = array (
 		"IBLOCK_ID" => $arParams["NEWS_IBLOCK_ID"],
 		"ACTIVE" => "Y",
+		//array(
+		//	"LOGIC" => "OR",
+		//	array("<PROPERTY_RADIUS" => 50, "=PROPERTY_CONDITION" => "Y"),
+		//	array(">=PROPERTY_RADIUS" => 50, "!=PROPERTY_CONDITION" => "Y"),
+		//),
 	);
 	$arSortElems = array (
 		"NAME" => "ASC",
 	);
 	
+	$curUserNews = [];
+	$arResult["NEWS"] = array();
 	$rsElements = CIBlockElement::GetList($arSortElems, $arFilterElems, false, false, $arSelectElems);
 	while($arElement = $rsElements->GetNext()) {
 		$arResult["NEWS"][] = $arElement;
 		
-		echo "<pre>";print_r($arElement);echo "</pre>";
+		if ($arElement["PROPERTY_" . $arParams["AUTHOR_PROP_CODE"] . "_VALUE"] == $curUser["ID"]) {
+			$curUserNews[] = $arElement["ID"];
+		}
 	}
+	
+	foreach ($arResult["USERS"] as $userKey => $user) {
+		foreach ($arResult["NEWS"] as $news) {
+			if (
+				$user["ID"] == $curUser["ID"] && $user["ID"] == $news["PROPERTY_" . $arParams["AUTHOR_PROP_CODE"] . "_VALUE"]
+				|| $user["ID"] != $curUser["ID"] && !in_array($news["ID"], $curUserNews) && $user["ID"] == $news["PROPERTY_" . $arParams["AUTHOR_PROP_CODE"] . "_VALUE"]
+				) {
+				$arResult["USERS"][$userKey]["NEWS"][] = $news;
+			}
+		}
+	}
+	
+	echo "<pre>";print_r($curUserNews);echo "</pre>";
+	echo "<pre>";print_r($arResult["USERS"]);echo "</pre>";
 }
 
 $this->includeComponentTemplate();	

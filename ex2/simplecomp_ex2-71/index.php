114c05d28ebc150dd8362d40a59d4485148f1c6c
anattoliy90 <anattoliy90@gmail.com>
2020-10-06 10:27:25 +0300

    working on ex2-71

--- ex2/simplecomp_ex2-71/index.php
+++ ex2/simplecomp_ex2-71/index.php
@@ -2,16 +2,18 @@
 $APPLICATION->SetTitle("");
 
 $APPLICATION->IncludeComponent(
-	"molotok:simplecomp.exam_ex2-71",
-	"",
-	Array(
+	"molotok:simplecomp.exam_ex2-71", 
+	".default", 
+	array(
 		"CACHE_TIME" => "3600",
 		"CACHE_TYPE" => "A",
-		"CATALOG_IBLOCK_ID" => "",
+		"CATALOG_IBLOCK_ID" => "2",
 		"DETAIL_URL_TEMPLATE" => "",
-		"FIRM_IBLOCK_ID" => "",
-		"PROPERTY_CODE" => ""
-	)
+		"FIRM_IBLOCK_ID" => "5",
+		"PROPERTY_CODE" => "FIRM",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
 );
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
--- local/components/molotok/simplecomp.exam_ex2-71/component.php
+++ local/components/molotok/simplecomp.exam_ex2-71/component.php
@@ -13,10 +13,17 @@ if($this->StartResultCache(false, ($USER->GetGroups()))) {
 		return;
 	}
 	
-	$rsIBlockElement = CIBlockElement::GetList([], [], false, false, []);
-	// $rsIBlockElement->SetUrlTemplates($arParams["DETAIL_URL"]);
-	while ($arResult = $rsIBlockElement->GetNext()) {
+	$firm = [];
 	
+	$res = CIBlockElement::GetList([], ['IBLOCK_ID' => $arParams['FIRM_IBLOCK_ID']], false, false, ['ID', 'NAME']);
+	// $rsIBlockElement->SetUrlTemplates($arParams["DETAIL_URL"]);
+	while ($ob = $res->GetNext()) {
+		$firm[$ob['ID']] = $ob['NAME'];
+	}
+	
+	$result = CIBlockElement::GetList([], ['IBLOCK_ID' => $arParams['CATALOG_IBLOCK_ID'], 'PROPERTY_' . $arParams['PROPERTY_CODE'] => array_keys($firm)], false, false, ['ID', 'NAME', 'PROPERTY_' . $arParams['PROPERTY_CODE'], 'PROPERTY_PRICE', 'PROPERTY_MATERIAL', 'PROPERTY_ARTNUMBER', 'DETAIL_PAGE_URL']);
+	while ($obj = $result->GetNext()) {
+		echo '<pre>';print_r($obj);echo '</pre>';
 	}
 	
 	$this->SetResultCacheKeys([]);

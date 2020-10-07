ed9cd2df3431e05e56a6f87da0d94c79ccdedabf
anattoliy90 <anattoliy90@gmail.com>
2020-10-07 10:28:55 +0300

    ex2-71 done

--- local/components/molotok/simplecomp.exam_ex2-71/component.php
+++ local/components/molotok/simplecomp.exam_ex2-71/component.php
@@ -2,30 +2,57 @@
 
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
+$arResult['FIRM_COUNT'] = 0;
+
 if(!isset($arParams["CACHE_TIME"])) {
 	$arParams["CACHE_TIME"] = 3600;
 }
 
-if($this->StartResultCache(false, ($USER->GetGroups()))) {
-	if(!CModule::IncludeModule("iblock")) {
+if ($this->StartResultCache(false, $USER->GetGroups())) {
+	if (!CModule::IncludeModule("iblock")) {
 		$this->AbortResultCache();
 		ShowError(GetMessage("IBLOCK_MODULE_NOT_INSTALLED"));
 		return;
 	}
 	
-	$firm = [];
+	$arResult['FIRMS'] = [];
+	$items = [];
 	
-	$res = CIBlockElement::GetList([], ['IBLOCK_ID' => $arParams['FIRM_IBLOCK_ID']], false, false, ['ID', 'NAME']);
-	// $rsIBlockElement->SetUrlTemplates($arParams["DETAIL_URL"]);
+	$res = CIBlockElement::GetList([], ['IBLOCK_ID' => $arParams['FIRM_IBLOCK_ID'], 'ACTIVE' => 'Y', 'CHECK_PERMISSIONS' => 'Y'], false, false, ['ID', 'NAME']);
+	// $rsIBlockElement->SetUrlTemplates($arParams['DETAIL_URL']);
 	while ($ob = $res->GetNext()) {
-		$firm[$ob['ID']] = $ob['NAME'];
+		$arResult['FIRMS'][] = [
+			'ID' => $ob['ID'],
+			'NAME' => $ob['NAME'],					   
+		];
 	}
 	
-	$result = CIBlockElement::GetList([], ['IBLOCK_ID' => $arParams['CATALOG_IBLOCK_ID'], 'PROPERTY_' . $arParams['PROPERTY_CODE'] => array_keys($firm)], false, false, ['ID', 'NAME', 'PROPERTY_' . $arParams['PROPERTY_CODE'], 'PROPERTY_PRICE', 'PROPERTY_MATERIAL', 'PROPERTY_ARTNUMBER', 'DETAIL_PAGE_URL']);
+	$arResult['FIRM_COUNT'] = count($arResult['FIRMS']);
+	$firmId = array_column($arResult['FIRMS'], 'ID');
+	
+	$result = CIBlockElement::GetList([], ['IBLOCK_ID' => $arParams['CATALOG_IBLOCK_ID'], 'PROPERTY_' . $arParams['PROPERTY_CODE'] => $firmId, 'ACTIVE' => 'Y', 'CHECK_PERMISSIONS' => 'Y'], false, false, ['ID', 'NAME', 'PROPERTY_' . $arParams['PROPERTY_CODE'], 'PROPERTY_PRICE', 'PROPERTY_MATERIAL', 'PROPERTY_ARTNUMBER', 'DETAIL_PAGE_URL']);
 	while ($obj = $result->GetNext()) {
-		echo '<pre>';print_r($obj);echo '</pre>';
+		$items[] = [
+			'ID' => $obj['ID'],
+			'NAME' => $obj['NAME'],
+			'FIRM_ID' => $obj['PROPERTY_FIRM_VALUE'],
+			'PRICE' => $obj['PROPERTY_PRICE_VALUE'],
+			'MATERIAL' => $obj['PROPERTY_MATERIAL_VALUE'],
+			'ARTNUMBER' => $obj['PROPERTY_ARTNUMBER_VALUE'],
+			'DETAIL_PAGE_URL' => $obj['DETAIL_PAGE_URL'],
+		];
 	}
 	
-	$this->SetResultCacheKeys([]);
+	foreach ($arResult['FIRMS'] as $key => $firm) {
+		foreach ($items as $item) {
+			if ($firm['ID'] == $item['FIRM_ID']) {
+				$arResult['FIRMS'][$key]['ITEMS'][] = $item;
+			}
+		}
+	}
+	
+	$this->SetResultCacheKeys(['FIRM_COUNT']);
 	$this->IncludeComponentTemplate();
 }
+
+$APPLICATION->SetTitle(GetMessage('SECTIONS') . $arResult['FIRM_COUNT']);
--- local/components/molotok/simplecomp.exam_ex2-71/templates/.default/template.php
+++ local/components/molotok/simplecomp.exam_ex2-71/templates/.default/template.php
@@ -1,30 +1,14 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
-$frame = $this->createFrame()->begin('');
-?>
-<div class="photo-random">
-	<?if(is_array($arResult["PICTURE"])):?>
-		<a href="<?=$arResult["DETAIL_PAGE_URL"]?>"><img
-				border="0"
-				src="<?=$arResult["PICTURE"]["SRC"]?>"
-				width="<?=$arResult["PICTURE"]["WIDTH"]?>"
-				height="<?=$arResult["PICTURE"]["HEIGHT"]?>"
-				alt="<?=$arResult["PICTURE"]["ALT"]?>"
-				title="<?=$arResult["PICTURE"]["TITLE"]?>"
-				/></a><br />
-	<?endif?>
-	<a href="<?=$arResult["DETAIL_PAGE_URL"]?>"><?=$arResult["NAME"]?></a>
-</div>
-<?
-$frame->end();
-?>
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); ?>
+
+<? if (!empty($arResult['FIRMS'])): ?>
+	<? foreach ($arResult['FIRMS'] as $firm): ?>
+		<ul>
+			<li><b><?= $firm['NAME'] ?></b></li>
+			<ul>
+				<? foreach ($firm['ITEMS'] as $item): ?>
+					<li><?= $item['NAME'] ?> - <?= $item['PRICE'] ?> - <?= $item['MATERIAL'] ?> - <?= $item['ARTNUMBER'] ?> - <?= $item['DETAIL_PAGE_URL'] ?></li>
+				<? endforeach; ?>
+			</ul>
+		</ul>
+	<? endforeach; ?>
+<? endif; ?>

54a13d0518902f28f0ce432e69d9117a6374406c
anattoliy90 <anattoliy90@gmail.com>
2020-10-09 10:17:22 +0300

    ex2-49 done

--- local/components/molotok/simplecomp.exam_ex2-71/component.php
+++ local/components/molotok/simplecomp.exam_ex2-71/component.php
@@ -3,9 +3,20 @@
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 $arResult['FIRM_COUNT'] = 0;
+$addFilter = [];
 
 if(!isset($arParams["CACHE_TIME"])) {
 	$arParams["CACHE_TIME"] = 3600;
+}
+
+if (isset($_REQUEST['F'])) {
+	$arParams["CACHE_TIME"] = 0;
+	
+	$addFilter[] = [
+		'LOGIC' => 'OR',
+		['<=PROPERTY_PRICE' => 1700, 'PROPERTY_MATERIAL' => 'Дерево, ткань'],
+		['<PROPERTY_PRICE' => 1500, 'PROPERTY_MATERIAL' => 'Металл, пластик'],
+	];
 }
 
 if ($this->StartResultCache(false, $USER->GetGroups())) {
@@ -28,8 +39,18 @@ if ($this->StartResultCache(false, $USER->GetGroups())) {
 	
 	$arResult['FIRM_COUNT'] = count($arResult['FIRMS']);
 	$firmId = array_column($arResult['FIRMS'], 'ID');
+	$filter = [
+		'IBLOCK_ID' => $arParams['CATALOG_IBLOCK_ID'],
+		'PROPERTY_' . $arParams['PROPERTY_CODE'] => $firmId,
+		'ACTIVE' => 'Y',
+		'CHECK_PERMISSIONS' => 'Y',
+	];
 	
-	$result = CIBlockElement::GetList(['NAME' => 'ASC', 'SORT' => 'ASC'], ['IBLOCK_ID' => $arParams['CATALOG_IBLOCK_ID'], 'PROPERTY_' . $arParams['PROPERTY_CODE'] => $firmId, 'ACTIVE' => 'Y', 'CHECK_PERMISSIONS' => 'Y'], false, false, ['ID', 'NAME', 'PROPERTY_' . $arParams['PROPERTY_CODE'], 'PROPERTY_PRICE', 'PROPERTY_MATERIAL', 'PROPERTY_ARTNUMBER', 'DETAIL_PAGE_URL']);
+	if (!empty($addFilter)) {
+		$filter = array_merge($filter, $addFilter);
+	}
+	
+	$result = CIBlockElement::GetList(['NAME' => 'ASC', 'SORT' => 'ASC'], $filter, false, false, ['ID', 'NAME', 'PROPERTY_' . $arParams['PROPERTY_CODE'], 'PROPERTY_PRICE', 'PROPERTY_MATERIAL', 'PROPERTY_ARTNUMBER', 'DETAIL_PAGE_URL']);
 	$result->SetUrlTemplates($arParams['DETAIL_URL_TEMPLATE']);
 	while ($obj = $result->GetNext()) {
 		$items[] = [
